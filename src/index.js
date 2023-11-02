1ac08d41f8dee7fa762f3e7dd7dbe78ecca2ca30
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-11-02 10:10:35 +0530

    Added function to compare the incoming password with encrypted password

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const apiRoutes = require('./routes/index');
 const { User } = require('./models/index');
 const bcrypt = require ('bcrypt');
 
+const UserService = require('./services/user-service');
+
 
 const prepareAndStartServer = () => {
          
@@ -20,11 +22,19 @@ const prepareAndStartServer = () => {
      
         app.listen( PORT , async () => {
         console.log(` Server Started at port : ${PORT} `);
-        const incomingpassword = '123456';
+        //const incomingpassword = '123456';
+      
+         
+        // printing the new token --------->
+    //    const service = new UserService();
+    //    const newToken = service.createToken({ email: 'mohit@admin.com', id: 1});
+    //    console.log( "new token is ", newToken);
 
-
-
-
+       // doing token validation ---------->
+    //    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vaGl0QGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2OTg4OTkxNTIsImV4cCI6MTY5ODg5OTE1NX0.eSf29QFGJImObepuBy6FFn5wQVrkpXLd7X3tCwQfmdk';
+    //    const response = service.verifyToken(token);
+    //    console.log(response);
+       
         // for comparison of password( hash password and user entered )
         // const user = await User.findByPk(2);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,10 @@
 const UserRepository = require ('../repository/user-repository');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+// for bringing key
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService {
     constructor() {
@@ -15,6 +21,35 @@ class UserService {
         }
      }
 
+     createToken(user){ 
+         try {
+             const result = jwt.sign(user, JWT_KEY,{expiresIn: 3} )// it expects a key => so we create it in .env file => it is any random string
+             return result;
+         } catch (error) {
+            console.log("Something went wrong in the token creation ");
+            throw error;
+         }
+     }
+
+     verifyToken( token ){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in the token validation ", error);
+            throw error;
+        }
+     }
+   
+     
+     checkPassword( userInputPlainPassword, encryptedPassword) {
+        try {
+             return bcrypt.compareSync( userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison ");
+            throw error;
+        }
+     }
 
 
 }
